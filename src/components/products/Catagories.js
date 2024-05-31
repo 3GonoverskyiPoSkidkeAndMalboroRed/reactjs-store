a1500d290ab1e9ce1527ff4d41e6cd0cedbe7cf8
Pavel Neverov <140485683+3GonoverskyiPoSkidkeAndMalboroRed@users.noreply.github.com>
2024-05-31 12:36:14 +0000

    исправлены зависимости

--- src/components/products/Catagories.js
+++ src/components/products/Catagories.js
@@ -1,68 +1,3 @@
-// import React from "react";
-// import { getProducts } from "../../utils/utils";
-// import { Thumbnail, CatBtn } from "../../styles/ProductStyle";
-// import { useNavigate } from "react-router-dom";
-
-// //--------------------Pass Router Dom Props
-// function withParams(Component) {
-//   return (props) => <Component {...props} navigate={useNavigate()} />;
-// }
-// //challenge figure out how to use navigate with class components.
-
-// class Catagories extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       categories: [],
-//       images: [
-//         "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg",
-//         "https://risovach.ru/upload/2014/08/mem/sudya-egorova_59461356_orig_.jpg",
-//         "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-//         "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg"
-//       ]
-//     };
-//   }
-//   componentDidMount() {
-//     getProducts("products/categories").then((res) => {
-//       this.setState({ categories: res });
-//     });
-//   }
-//   getRandomInt(max) {
-//     return Math.floor(Math.random() * max);
-//   }
-
-//   onClick = (category) => {
-//     this.props.navigate(`/catagory/${category}`);
-//     // this.getPicture(category);
-//   };
-
-//   render() {
-//     return (
-//       <>
-//         <div
-//           style={{
-//             display: "flex",
-//             flexWrap: "wrap",
-//             justifyContent: "center"
-//           }}
-//         >
-//           {this.state.categories.map((category, index) => (
-//             <div key={`${index}${category}`}>
-//               <CatBtn onClick={() => this.onClick(category)}>
-//                 <Thumbnail url={this.state.images[index]} />
-//                 {category}
-//               </CatBtn>
-//             </div>
-//           ))}
-//         </div>
-//       </>
-//     );
-//   }
-// }
-
-// export default withParams(Catagories);
-
-
 import React from "react";
 import { getProducts } from "../../utils/utils";
 import { Thumbnail, CatBtn } from "../../styles/ProductStyle";
@@ -126,3 +61,5 @@ class Catagories extends React.Component {
 }
 
 export default withParams(Catagories);
+
+
